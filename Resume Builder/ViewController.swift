986312915d295a1d9b0f9c0082f410029803cc9a
MyBook <MyBook@ketuls-macbook-pro.local>
2017-12-15 12:47:38 -0800

    Initial Commit

---
+++ Resume Builder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Resume Builder
+//
+//  Created by MyBook on 12/15/17.
+//  Copyright © 2017 Rutvik Desai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
